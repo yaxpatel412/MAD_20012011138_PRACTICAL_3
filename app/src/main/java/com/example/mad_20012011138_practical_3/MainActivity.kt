d87ccfecabb171b8eac9aca880bf845248f9a7ba
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-10-02 09:57:21 +0530

    MAD_PRACTICAL_3

--- app/src/main/java/com/example/mad_20012011138_practical_3/MainActivity.kt
+++ app/src/main/java/com/example/mad_20012011138_practical_3/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mad_20012011138_practical_3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_20012011138_practical_3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        Intent(this, LoginActivity::class.java).also {startActivity(it)}
     }
 }
+
